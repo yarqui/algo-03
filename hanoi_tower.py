eb67dac2351f8b40e1e43be249c154872c0b7e1b
yarqui <y.pelykh@gmail.com>
2025-04-09 16:35:49 -0400

    add hanoi_tower

---
+++ hanoi_tower.py
@@ -0,0 +1,57 @@
+from typing import Dict, List
+from colorama import Fore
+
+
+def hanoi(
+    n: int, source: str, target: str, auxiliary: str, towers: Dict[str, List[int]]
+):
+    """
+    Recursively moves disks from the source tower to the target tower,
+    using the auxiliary tower according to the Tower of Hanoi rules.
+
+    Args:
+        n (int): Number of disks to move.
+        source (str): The name of the source tower.
+        target (str): The name of the destination tower.
+        auxiliary (str): The name of the auxiliary tower.
+        towers (Dict[str, List[int]]): Dictionary representing all towers.
+    """
+    if n == 1:
+        disk = towers[source].pop()
+        print(f"popping {disk}")
+        towers[target].append(disk)
+        print(
+            f"{Fore.BLUE}Move disk {disk} from {Fore.GREEN}{source} {Fore.BLUE}to {Fore.GREEN}{target}"
+        )
+        print(f"Intermediate state: {towers}\n")
+        return
+
+    # move n-1 disks to auxiliary
+    hanoi(n - 1, source=source, target=auxiliary, auxiliary=target, towers=towers)
+
+    disk = towers[source].pop()
+    towers[target].append(disk)
+
+    print(
+        f"{Fore.BLUE}Moving disk {disk} from {Fore.GREEN}{source} {Fore.BLUE}to {Fore.GREEN}{target}"
+    )
+    print(f"Intermediate state: {towers}\n")
+
+    # move n-1 disks from auxiliary to target
+    hanoi(n - 1, source=auxiliary, target=target, auxiliary=source, towers=towers)
+
+
+def solve_hanoi(disks_quantity: int):
+    initial_tower = list(range(disks_quantity, 0, -1))
+    towers = {
+        "A": initial_tower,
+        "B": [],
+        "C": [],
+    }
+
+    print(f"{Fore.YELLOW}initial state: {towers}")
+    hanoi(disks_quantity, "A", "C", "B", towers)
+    print(f"{Fore.YELLOW}Final state: {towers}{Fore.RESET}")
+
+
+solve_hanoi(6)
